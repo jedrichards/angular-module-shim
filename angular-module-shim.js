7a817b9cabed9690138733a9973f218d1a894ec1
jedrichards <jedrichards@gmail.com>
2014-04-25 16:22:16 +0100

    Making sure requires argument is never undefined during module creation

--- angular-module-shim.js
+++ angular-module-shim.js
@@ -11,12 +11,13 @@
 
     angular.module = function(name,requires,configFn) {
 
+        var requires = requires || [];
         var registered = hash[name];
         var module;
 
         if ( registered ) {
             module = origFn(name);
-            if ( requires ) module.requires.push.apply(module.requires,requires);
+            module.requires.push.apply(module.requires,requires);
         } else {
             hash[name] = true;
             module = origFn(name,requires,configFn);
